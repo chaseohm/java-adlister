281a758af27f1f32ab57313b6c498f6e1bdb005d
admin <chaseohm@gmail.com>
2019-02-06 13:05:04 -0600

    ads created and implemented

---
+++ src/main/java/AdServlet.java
@@ -0,0 +1,25 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name="AdServlet", urlPatterns = "/ads")
+public class AdServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        // get all the quotes
+        List<Ad> ads = DaoFactory.getAdsDao().all();
+
+        // bind the quotes to the request object
+        request.setAttribute("ads", ads);
+
+        // send the response along w/ the data
+        request.getRequestDispatcher("/ads/index.jsp").forward(request, response);
+
+    }
+}
