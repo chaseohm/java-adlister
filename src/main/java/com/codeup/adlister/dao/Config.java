3c953be43d4d6e5c21cb8ae71ad5a843b1a0c44e
admin <chaseohm@gmail.com>
2019-02-13 10:15:04 -0600

    working

---
+++ src/main/java/com/codeup/adlister/dao/Config.java
@@ -0,0 +1,13 @@
+package com.codeup.adlister.dao;
+
+class Config {
+    public String getUrl() {
+        return "jdbc:mysql://adlister_db";
+    }
+    public String getUsername() {
+        return "admin";
+    }
+    public String getPassword() {
+        return "password";
+    }
+}
